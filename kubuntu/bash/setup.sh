e4a6b2b4984dbd6396346452884baa7c2d570289
Camilo Bermudez <camilobermudez85@gmail.com>
2018-03-02 00:32:05 -0500

    Bash aliases added

--- kubuntu/bash/setup.sh
+++ kubuntu/bash/setup.sh
@@ -4,15 +4,28 @@ set -x
 
 sudo apt-get install fonts-powerline
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pushd "${script_dir}"
 
-pushd powerline-shell
+pushd "${script_dir}/powerline-shell"
 cp ../powerline-shell-custom-theme.py powerline_shell/themes/custom.py
 python setup.py install --user
 rm powerline_shell/themes/custom.py
 popd
 
-echo -e "\n# Bash powerline\nif [ -f $(pwd)/bash-powerline.sh ]; then\n\tsource $(pwd)/bash-powerline.sh\nfi\n" >> ~/.bashrc
-ln -s $(pwd)/powerline-shell.json ~/.powerline-shell.json
+cat << EOF >> ~/.bashrc
 
-popd
+# Bash powerline
+if [ -f ${script_dir}/bash-powerline.sh ]; then
+	source ${script_dir}/bash-powerline.sh
+fi
+EOF
+ln -s ${script_dir}/powerline-shell.json ~/.powerline-shell.json
+
+cat << EOF >> ~/.bashrc
+
+# Aliases configuration
+if [ -d ${script_dir}/aliases ]; then
+	for f in ${script_dir}/aliases/*; do
+		source "\$f"
+	done
+fi
+EOF
